2da5ccba72e54c524c174172799678aa7d167324
youth <dcancer17@gmail.com>
2023-05-20 14:03:02 +0900

    230520

---
+++ StartBoot/src/main/java/com/naver/dcancer/StartBootApplication.java
@@ -0,0 +1,13 @@
+package com.naver.dcancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBootApplication.class, args);
+	}
+
+}
