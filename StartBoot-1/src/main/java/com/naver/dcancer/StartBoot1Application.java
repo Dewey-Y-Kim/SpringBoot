e89df88ddbee769041c9a0eb2973a332f85ebde8
youth <dcancer17@gmail.com>
2023-05-20 16:56:20 +0900

    230520

---
+++ StartBoot-1/src/main/java/com/naver/dcancer/StartBoot1Application.java
@@ -0,0 +1,13 @@
+package com.naver.dcancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBoot1Application.class, args);
+	}
+
+}
---
+++ StartBoot-1/src/test/java/com/naver/dcancer/StartBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.dcancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
